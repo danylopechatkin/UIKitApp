a2e21a4e8fe4aafc386398be3a73b16fc30b029e
Danylo <pechatkin.work@gmail.com>
2022-05-25 19:01:46 +0200

    Initial Commit

---
+++ UIKitApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitApp
+//
+//  Created by Danylo Pechatkin on 25/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
