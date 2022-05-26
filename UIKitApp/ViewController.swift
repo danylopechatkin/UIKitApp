68787f0dbf7079c6381bcdd46d91b432610d9f1b
Danylo <pechatkin.work@gmail.com>
2022-05-26 22:16:43 +0200

    Lesson / Slider,TextField,Segmented

--- UIKitApp/ViewController.swift
+++ UIKitApp/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var textField: UITextField!
     
     
     
@@ -20,18 +22,67 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
       
-        mainLabel.text = ""
-        mainLabel.font = mainLabel.font.withSize(35)
+       
+        mainLabel.font = mainLabel.font.withSize(30)
         mainLabel.textAlignment = .center
         mainLabel.numberOfLines = 2
         
         segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: false)
         
         
+        slider.value = 1
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        slider.minimumTrackTintColor = .black
+        slider.maximumTrackTintColor = .white
+        slider.thumbTintColor = .blue
+        
+        mainLabel.text = String(slider.value)
+        
+        
     }
 
     @IBAction func segmentedControlAction() {
+        
+        
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            mainLabel.text = "The First Segment is selected"
+            mainLabel.textColor = .red
+        case 1:
+            mainLabel.text = "The Second Segment is selected"
+            mainLabel.textColor = .blue
+       default:
+            mainLabel.text = "The Third Segment is selected"
+            mainLabel.textColor = .yellow
+        }
+        
+    }
+    @IBAction func slideraction() {
+        
+        mainLabel.text = String(slider.value)
+        let sliderValue = CGFloat(slider.value)
+        view.backgroundColor = view.backgroundColor?.withAlphaComponent(sliderValue)
     }
     
+    @IBAction func doneButtonPressed() {
+        guard let inputText = textField.text, !inputText.isEmpty else {
+            print("Text Field is empty")
+            return
+        }
+        
+        if let _ = Double(inputText){
+            print("Wrong format")
+            return
+        }
+        
+        mainLabel.text = textField.text
+    }
+
 }
 
+//MARK: - Private Methods 
+
+extension ViewController {
+    
+}
