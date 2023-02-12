5682d68b59e0b49c0fcf207da641b910c912180f
Danylo <pechatkin.work@gmail.com>
2023-02-12 18:33:12 +0100

    SetupViewContoller

--- UIKitApp/ViewController.swift
+++ UIKitApp/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var mainLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func segmentedControl2() {
+    }
+    
 }
 
