3013d28ddeea28026bb483425e0e162be6eff6f5
Danylo <pechatkin.work@gmail.com>
2023-02-17 23:04:58 +0100

    Full Setup Lesson Work

--- UIKitApp/ViewController.swift
+++ UIKitApp/ViewController.swift
@@ -11,20 +11,23 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var slider: UISlider!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       
-        mainLabel.font = mainLabel.font.withSize(25)
+        
+        mainLabel.font = mainLabel.font.withSize(35)
         mainLabel.textAlignment = .center
         mainLabel.textColor = .purple
-        mainLabel.numberOfLines = 2
+        mainLabel.numberOfLines = 3
         
         segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: false)
         
-        slider.value = 0.4
+        slider.value = 1
         slider.minimumValue = 0
         slider.maximumValue = 1
         slider.minimumTrackTintColor = .yellow
@@ -56,6 +59,33 @@ class ViewController: UIViewController {
         mainLabel.text = String(slider.value)
         let sliderValue = CGFloat (slider.value)
         view.backgroundColor = view.backgroundColor?.withAlphaComponent(sliderValue)
+    }
+    
+    
+    
+    @IBAction func doneButtonTest() {
+        guard let inputText =  textField.text, !inputText.isEmpty else {
+            showAlert(title: "Textfield is empty", message: "Please enter your name")
+            return }
+        
+        if let _ = Double(inputText) {
+            showAlert(title: "Wrong foramt", message: "Please enter your name")
+            return
+        }
+        mainLabel.text = textField.text
+        textField.text = ""
+    }}
+
+// MARK: - PRivate MEthod
+extension ViewController {
+    private func showAlert(title: String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default) { _ in
+            self.textField.text = ""
+            
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
     }
 }
 
